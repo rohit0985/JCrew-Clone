709992430d769386418ba9370d45ce6344b377bf
rohit0985 <rohitsharma4681@gmail.com>
2022-11-12 14:51:20 +0530

    added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,4 @@
 
-import logo from "./logo.svg";
-import "./App.css";
-import NavBar from "./Components/NavBar";
-import Footer from "./Components/Footer";
-import MainRoutes from "./Routes/MainRoutes";
-import Recomended from "./Components/Recomended";
-import ShopStories from "./Components/ShopStories";
-import MoreYouNeed from "./Components/MoreYouNeed";
-import ProductBanner from "./Components/ProductBanner";
-
 import logo from './logo.svg';
 import './App.css';
 import Footer from './Components/Footer';
@@ -21,7 +11,7 @@ function App() {
   return (
     <div className="App">
 
-      <NavBar />
+      <Navbar />
        <MainRoutes/>
       <Footer/>
 
