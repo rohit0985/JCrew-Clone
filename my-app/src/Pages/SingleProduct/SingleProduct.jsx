e4255dbc413b490fee04577e0b184497b9977907
rohit0985 <rohitsharma4681@gmail.com>
2022-12-22 19:45:46 +0530

    responsiveness added to the single product page

--- my-app/src/Pages/SingleProduct/SingleProduct.jsx
+++ my-app/src/Pages/SingleProduct/SingleProduct.jsx
@@ -19,7 +19,7 @@ const SingleProduct = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
   const [image, setImage] = useState("");
-  const [size, setSize] = useState("")
+  const [size, setSize] = useState("");
 
   const product = useSelector(
     (reduxStore) => reduxStore.SingleProductReducer.product
@@ -55,7 +55,6 @@ const SingleProduct = () => {
             </div>
           </div>
 
-
           <div className={styles.right}>
             <p className={styles.category}>{product.category}</p>
             <h3 className={styles.name}>{product.name}</h3>
@@ -76,40 +75,98 @@ const SingleProduct = () => {
                 <span>Color:</span>
                 {product.Product_colors[0].colorName}
               </p>
-              <img  
-              className={
-                styles.colorImg
-              }
-              src={product.Product_colors[0].images[0]} 
-              alt={product.Product_colors[0].colorName} />
+              <img
+                className={styles.colorImg}
+                src={product.Product_colors[0].images[0]}
+                alt={product.Product_colors[0].colorName}
+              />
             </div>
 
-<div className={styles.size}>
+            <div className={styles.size}>
+              <div className={styles.sizeInfo}>
+                <div>
+                  <span>Size:</span>
+                  <p>{size ? size : "Select a Size"}</p>
+                </div>
+                <p>All sizes are US sizes.</p>
+              </div>
 
-<div className={styles.sizeInfo}>
-<div>
-<span>Size:</span>
-  <p>{size ? size : "Select a Size"}</p>
+              <div className={styles.sizeWrapper}>
+                {product.Product_colors[0].availableSize.map((el) => (
+                  <div
+                    onClick={() => setSize(el)}
+                    className={
+                      size === el ? styles.choosedSize : styles.chooseSize
+                    }
+                  >
+                    {el}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className={styles.quantity}>
+              <span>Quantity</span>
+              <select name="" id="">
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+                <option value="">5</option>
+                <option value="">6</option>
+                <option value="">7</option>
+                <option value="">8</option>
+                <option value="">9</option>
+              </select>
+            </div>
+
+            <div className={styles.btn}>
+            <i>Prices include duties and taxes.</i>
+            <div>
+              <button  className={styles.atb}>
+                ADD TO BAG
+              </button>
+              <button className={styles.atc}>
+                <BsSuitHeart size={20} />
+              </button>
+            </div>
+          </div>
+
+
+          <hr />
+
+<div className={styles.description}>
+  <h4 >Product Details</h4>
+  <p>{product.productDetails.desc}</p>
+  <ul>
+    {product?.productDetails.highlightPoints.map((el, i) => (
+      <li key={i}>{el}</li>
+    ))}
+  </ul>
 </div>
-  <p>All sizes are US sizes.</p>
-</div>
 
 
-<div className={styles.sizeWrapper}>
-  {
-    product.Product_colors[0].availableSize.map((el)=> <div 
-    onClick={()=>setSize(el)} 
-    className={size === el ? styles.choosedSize : styles.chooseSize}>{el}</div> )
-  }
-</div>
+
+<div className={styles.more}>
+            <a href="https://www.jcrew.com/s/corporate-responsibility">Learn More</a>
+            <a href="#">Shop All Re-imagined</a>
+          </div>
+
+          <hr />
 
 
-</div>
 
 
-<div className={styles.quantity}>
-<label htmlFor=""></label>
-</div>
+          <div className={styles.social}>
+            <span>Share</span>
+            <BsPinterest />
+            <AiFillFacebook />
+            <BsTwitter />
+            <SlSocialTumblr />
+          </div>
+
+
+
 
           </div>
         </div>
